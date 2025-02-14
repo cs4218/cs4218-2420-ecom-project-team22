3b12d3c15554d052986191925625598559b1ad32
Kenneth Ruan <kennethjruan@gmail.com>
2025-02-14 19:01:04 +0800

    Add remaining category controller tests (single, get all, delete) and added additional database error tests

--- controllers/categoryController.test.js
+++ controllers/categoryController.test.js
@@ -1,5 +1,11 @@
 import { jest, expect, test, describe } from "@jest/globals";
-import { createCategoryController, updateCategoryController } from "./categoryController";
+import {
+  createCategoryController,
+  updateCategoryController,
+  categoryControlller,
+  singleCategoryController,
+  deleteCategoryCOntroller,
+} from "./categoryController";
 import categoryModel from "../models/categoryModel";
 import slugify from "slugify";
 
@@ -9,7 +15,7 @@ describe("Create Category Controller Tests", () => {
   let req, res;
 
   beforeEach(() => {
-    jest.clearAllMocks();
+    jest.resetAllMocks();
     res = {
       status: jest.fn().mockReturnThis(),
       send: jest.fn((data) => data),
@@ -23,7 +29,8 @@ describe("Create Category Controller Tests", () => {
 
     await createCategoryController(req, res);
 
-    expect(res.send).toHaveBeenCalledWith(
+    expect(res.status).toBeCalledWith(201);
+    expect(res.send).toBeCalledWith(
       expect.objectContaining({
         success: true,
       })
@@ -57,12 +64,41 @@ describe("Create Category Controller Tests", () => {
     await createCategoryController(req, res);
     expect(categoryModel.findOne).not.toHaveBeenCalled();
   });
+
+  test("error in checking existing category is handled gracefully", async () => {
+    categoryModel.findOne = jest.fn().mockRejectedValue(new Error("Database error"));
+    console.log = jest.fn((data) => data);
+    req = { body: { name: "health" } };
+
+    await createCategoryController(req, res);
+    expect(res.send).toBeCalledWith(500);
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: false,
+      })
+    );
+  });
+
+  test("database error in saving category is handled gracefully", async () => {
+    categoryModel.findOne = jest.fn().mockResolvedValue(null);
+    categoryModel.prototype.save = jest.fn().mockRejectedValue(new Error("Database error"));
+    console.log = jest.fn((data) => data);
+    req = { body: { name: "health" } };
+
+    await createCategoryController(req, res);
+    expect(res.send).toBeCalledWith(500);
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: false,
+      })
+    );
+  });
 });
 
 describe("Update Category Controller Tests", () => {
   let res, req;
   beforeEach(() => {
-    jest.clearAllMocks();
+    jest.resetAllMocks();
     res = {
       status: jest.fn().mockReturnThis(),
       send: jest.fn((data) => data),
@@ -89,6 +125,7 @@ describe("Update Category Controller Tests", () => {
       expect.objectContaining({ new: true })
     );
 
+    expect(res.status).toBeCalledWith(200);
     expect(res.send).toBeCalledWith(
       expect.objectContaining({
         success: true,
@@ -97,17 +134,195 @@ describe("Update Category Controller Tests", () => {
     );
   });
 
-  test("Updating a non-existent category results in a failed update", async () => {
+  test("updating a non-existent category results in a failed update", async () => {
     req = {
       body: { name: "clothing" },
-      param: { id: "ABC" },
+      params: { id: "ABC" },
     };
 
     categoryModel.findByIdAndUpdate = jest.fn().mockReturnValue(null);
 
     await updateCategoryController(req, res);
 
-    expect(res.send).toHaveBeenCalledWith(
+    expect(res.status).toBeCalledWith(500);
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: false,
+      })
+    );
+  });
+});
+
+describe("All Category Controller Tests", () => {
+  let res, req;
+  beforeEach(() => {
+    jest.resetAllMocks();
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn((data) => data),
+    };
+  });
+
+  test("All categories from database are returned", async () => {
+    const mockCategories = [
+      { name: "accessories", slug: slugify("accessories") },
+      { name: "clothing", slug: slugify("clothing") },
+    ];
+
+    req = {};
+    categoryModel.find = jest.fn().mockResolvedValue(mockCategories);
+
+    await categoryControlller(req, res);
+
+    expect(res.status).toBeCalledWith(200);
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: true,
+        category: mockCategories,
+      })
+    );
+  });
+
+  test("database error occurring in category retrieval is handled gracefully", async () => {
+    req = {};
+    console.log = jest.fn((data) => data);
+    categoryModel.find = jest.fn().mockRejectedValue(new Error("Database error"));
+
+    await categoryControlller(req, res);
+
+    expect(res.status).toBeCalledWith(500);
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: false,
+      })
+    );
+  });
+});
+
+describe("Single Category Controller Tests", () => {
+  let res, req;
+  beforeEach(() => {
+    jest.resetAllMocks();
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn((data) => data),
+    };
+  });
+
+  test("retrieved category is returned in response", async () => {
+    const mockCategory = {
+      name: "toiletries",
+      slug: slugify("toiletries"),
+    };
+    req = {
+      params: {
+        slug: slugify("toiletries"),
+      },
+    };
+
+    categoryModel.findOne = jest.fn().mockResolvedValue(mockCategory);
+
+    await singleCategoryController(req, res);
+
+    expect(res.status).toBeCalledWith(200);
+    // The category from the db call is returned
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: true,
+        category: mockCategory,
+      })
+    );
+    // Returned category has the same slug as the request
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        category: expect.objectContaining({
+          slug: req.params.slug,
+        }),
+      })
+    );
+  });
+
+  test("retrieving non-existent category should result in an error", async () => {
+    req = {
+      params: {
+        slug: "hippo",
+      },
+    };
+    categoryModel.findOne = jest.fn().mockResolvedValue(null);
+
+    await singleCategoryController(req, res);
+    expect(res.status).toBeCalledWith(500);
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: false,
+      })
+    );
+  });
+
+  test("database error in retrieving category is handled gracefully", async () => {
+    req = {
+      params: {
+        slug: "toiletries",
+      },
+    };
+    categoryModel.findOne = jest.fn().mockRejectedValue(new Error("database error"));
+
+    await singleCategoryController(req, res);
+
+    expect(res.status).toBeCalledWith(500);
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: false,
+      })
+    );
+  });
+});
+
+describe("Delete Category Controller Tests", () => {
+  let res, req;
+  beforeEach(() => {
+    jest.resetAllMocks();
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn((data) => data),
+    };
+  });
+
+  test("valid delete results in success and returns with deleted category", async () => {
+    const mockCategory = {
+      name: "toiletries",
+      slug: slugify("toiletries"),
+    };
+    req = {
+      params: {
+        id: "ABC",
+      },
+    };
+    categoryModel.findByIdAndDelete = jest.fn().mockResolvedValue(mockCategory);
+
+    await deleteCategoryCOntroller(req, res);
+
+    expect(res.status).toBeCalledWith(200);
+    expect(res.send).toBeCalledWith(
+      expect.objectContaining({
+        success: true,
+        category: mockCategory,
+      })
+    );
+  });
+
+  test("deleting non-existent category results in error", async () => {
+    req = {
+      params: {
+        id: "ABC",
+      },
+    };
+    categoryModel.findByIdAndDelete = jest.fn().mockResolvedValue(null);
+
+    await deleteCategoryCOntroller(req, res);
+
+    expect(res.status).toBeCalledWith(500);
+    expect(res.send).toBeCalledWith(
       expect.objectContaining({
         success: false,
       })
